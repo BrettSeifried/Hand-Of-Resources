008b8a521b979761cba4a9282aa4bfcbcfea4abf
Brett Seifried <seifriedbrett@gmail.com>
2022-03-17 09:51:59 -0700

    passing findAll for mics

--- __tests__/mics.test.js
+++ __tests__/mics.test.js
@@ -2,6 +2,20 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Mic = require('../lib/models/Mics');
+
+async function createMic({ name, input, price }) {
+  const { rows } = await pool.query(
+    `INSERT INTO 
+      mics(name, input, price) 
+    VALUES 
+      ($1, $2, $3) 
+    RETURNING 
+      *;`,
+    [name, input, price]
+  );
+  return new Mic(rows[0]);
+}
 
 describe('hands-of-resources routes', () => {
   beforeEach(() => {
@@ -21,5 +35,12 @@ describe('hands-of-resources routes', () => {
     const resp = await request(app).post('/api/v1/mics').send(expected);
 
     expect(resp.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it('fetches all data', async () => {
+    const expected = await Mic.findAll();
+    const resp = await request(app).get('/api/v1/mics');
+
+    expect(resp.body).toEqual(expected);
   });
 });

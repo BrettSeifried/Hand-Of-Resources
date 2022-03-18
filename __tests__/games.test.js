712f966cab67436572baac43b3e15a5370903574
Brett Seifried <seifriedbrett@gmail.com>
2022-03-18 15:44:37 -0700

    finished get list of all

--- __tests__/games.test.js
+++ __tests__/games.test.js
@@ -2,6 +2,15 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Game = require('../lib/models/Games');
+
+async function createGame({ name, type, rating }) {
+  const { rows } = await pool.query(
+    'INSERT INTO games(name, type, rating) VALUES ($1, $2, $3) RETURNING *;',
+    [name, type, rating]
+  );
+  return new Game(rows[0]);
+}
 
 describe('hands-of-resources routes', () => {
   beforeEach(() => {
@@ -23,5 +32,23 @@ describe('hands-of-resources routes', () => {
       type: 'RPG',
       rating: 9,
     });
+  });
+
+  it('should list all games', async () => {
+    await createGame({
+      name: 'Elden Ring',
+      type: 'RPG',
+      rating: 9,
+    });
+    const resp = await request(app).get('/api/v1/games');
+
+    expect(resp.body).toEqual([
+      {
+        id: expect.any(String),
+        name: 'Elden Ring',
+        type: 'RPG',
+        rating: 9,
+      },
+    ]);
   });
 });

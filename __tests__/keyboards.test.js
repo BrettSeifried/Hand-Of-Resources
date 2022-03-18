3a51218dafc24beb014a428bcbd71f012c3f0d6d
Brett Seifried <seifriedbrett@gmail.com>
2022-03-18 12:41:54 -0700

    passing findAll for keyboards

--- __tests__/keyboards.test.js
+++ __tests__/keyboards.test.js
@@ -2,6 +2,15 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Keyboard = require('../lib/models/Keyboards');
+
+async function createKeyboard({ brand, name, mech }) {
+  const { rows } = await pool.query(
+    'INSERT INTO keyboards(brand, name, mech) VALUES ($1, $2, $3) RETURNING *;',
+    [brand, name, mech]
+  );
+  return new Keyboard(rows[0]);
+}
 
 describe('hands-of-resources routes', () => {
   beforeEach(() => {
@@ -22,5 +31,23 @@ describe('hands-of-resources routes', () => {
       name: 'G915',
       mech: true,
     });
+  });
+
+  it('list all keyboards', async () => {
+    await createKeyboard({
+      brand: 'Logitech',
+      name: 'G915',
+      mech: true,
+    });
+    const resp = await request(app).get('/api/v1/keyboards');
+
+    expect(resp.body).toEqual([
+      {
+        id: expect.any(String),
+        brand: 'Logitech',
+        name: 'G915',
+        mech: true,
+      },
+    ]);
   });
 });

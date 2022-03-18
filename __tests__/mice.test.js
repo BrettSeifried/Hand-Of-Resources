4beac4e38010cd5fa6a593c76377fd59da7f487a
Brett Seifried <seifriedbrett@gmail.com>
2022-03-18 10:38:04 -0700

    passing findAll test for mice

--- __tests__/mice.test.js
+++ __tests__/mice.test.js
@@ -2,6 +2,15 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Mice = require('../lib/models/Mice');
+
+async function createMouse({ brand, name, price }) {
+  const { rows } = await pool.query(
+    'INSERT INTO mice(brand, name, price) VALUES ($1, $2, $3) RETURNING *;',
+    [brand, name, price]
+  );
+  return new Mice(rows[0]);
+}
 
 describe('hands-of-resources routes', () => {
   beforeEach(() => {
@@ -23,5 +32,19 @@ describe('hands-of-resources routes', () => {
       name: 'MX Master 3',
       price: 80,
     });
+  });
+
+  it('List all mics', async () => {
+    await createMouse({ brand: 'Logitech', name: 'MX Master 3', price: 80 });
+    const resp = await request(app).get('/api/v1/mice');
+
+    expect(resp.body).toEqual([
+      {
+        id: expect.any(String),
+        brand: 'Logitech',
+        name: 'MX Master 3',
+        price: 80,
+      },
+    ]);
   });
 });

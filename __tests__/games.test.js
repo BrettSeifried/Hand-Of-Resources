4ad4cf5c6a617d4e5ddbd31a169af9a0929daa90
Brett Seifried <seifriedbrett@gmail.com>
2022-03-18 16:09:08 -0700

    update by id Completed

--- __tests__/games.test.js
+++ __tests__/games.test.js
@@ -3,6 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 const Game = require('../lib/models/Games');
+const { findGameById } = require('../lib/models/Games');
 
 async function createGame({ name, type, rating }) {
   const { rows } = await pool.query(
@@ -35,7 +36,7 @@ describe('hands-of-resources routes', () => {
   });
 
   it('should list all games', async () => {
-    await createGame({
+    await Game.insert({
       name: 'Elden Ring',
       type: 'RPG',
       rating: 9,
@@ -61,5 +62,25 @@ describe('hands-of-resources routes', () => {
     const resp = await request(app).get(`/api/v1/games/${game.id}`);
 
     expect(resp.body).toEqual(game);
+  });
+
+  it('Update by ID', async () => {
+    const game = await createGame({
+      name: 'Elden Ring',
+      type: 'RPG',
+      rating: 9,
+    });
+    const resp = await request(app)
+      .patch(`/api/v1/orders/${game.id}`)
+      .send({ rating: 8 });
+    const expected = {
+      id: expect.any(String),
+      name: 'Elden Ring',
+      type: 'RPG',
+      rating: 8,
+    };
+
+    expect(res.body).toEqual(expected);
+    expect(await findGameById);
   });
 });

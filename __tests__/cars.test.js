a0fee6f3823b5498f20126fcc5cfe7f4bf6b592e
Brett Seifried <seifriedbrett@gmail.com>
2022-03-18 17:05:30 -0700

    insert cars passing test

--- __tests__/cars.test.js
+++ __tests__/cars.test.js
@@ -11,4 +11,16 @@ describe('hands-of-resources routes', () => {
   afterAll(() => {
     pool.end();
   });
+
+  it('should be able to create a car', async () => {
+    const resp = await request(app)
+      .post('/api/v1/cars')
+      .send({ name: 'Mclaren', model: '720s' });
+
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      name: 'Mclaren',
+      model: '720s',
+    });
+  });
 });
